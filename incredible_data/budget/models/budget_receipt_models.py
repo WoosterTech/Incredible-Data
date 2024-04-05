03b3b1711e5a6e263b85a85b5b4288fa550bf08f
Karl Wooster <karl.wooster@alleima.com>
2024-04-05 09:50:02 -0700

    feat(receipts): add models and signals for receipts, including merchants and very simple items

--- incredible_data/budget/models/budget_receipt_models.py
+++ incredible_data/budget/models/budget_receipt_models.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
+from django_extensions.db.fields import ShortUUIDField
 from django_extensions.db.models import TitleSlugDescriptionModel
 from djmoney.models.fields import MoneyField
 from model_utils.models import TimeStampedModel
@@ -25,6 +26,16 @@ class Merchant(TitleSlugDescriptionModel):
 
     def __str__(self) -> str:
         return f"{self.title}"
+
+
+class MerchantAlias(models.Model):
+    alias = models.CharField(_("alias"), max_length=50)
+    matched_merchant = models.ForeignKey(
+        Merchant, verbose_name=_("matched merchant"), on_delete=models.CASCADE
+    )
+
+    def __str__(self) -> str:
+        return f"{self.alias} <-> {self.matched_merchant}"
 
 
 class Receipt(TimeStampedModel, UserStampedModel):
@@ -61,3 +72,20 @@ class Receipt(TimeStampedModel, UserStampedModel):
         if self._state.adding:
             analyze_receipt_file.delay()
         return super().save(*args, **kwargs)
+
+
+class ReceiptItem(models.Model):
+    uuid = ShortUUIDField(primary_key=True, max_length=10)
+    product_code = models.CharField(_("product code or UPC"), max_length=50, blank=True)
+    description = models.CharField(_("description"), max_length=100, blank=True)
+    total_price = MoneyField(
+        _("total price"), max_digits=19, decimal_places=4, default=0
+    )
+    parent_receipt = models.ForeignKey(
+        Receipt, verbose_name=_("parent receipt"), on_delete=models.CASCADE
+    )
+
+    def __str__(self) -> str:
+        if self.product_code is not None or self.description is not None:
+            return f"Item({self.description or '(no description)'}, code={self.product_code or '(no UPC)'}, price={self.total_price})"  # noqa: E501
+        return f"unknown item, price={self.total_price}"
--- incredible_data/budget/receipt_services.py
+++ incredible_data/budget/receipt_services.py
@@ -5,9 +5,8 @@ from azure.ai.documentintelligence import DocumentIntelligenceClient
 from azure.core.credentials import AzureKeyCredential
 from celery import shared_task
 from django.conf import settings
-from pydantic import BaseModel
 
-from .models.budget_receipt_models import Receipt
+from .models.budget_receipt_models import Merchant, Receipt
 
 AZURE_ENDPOINT = settings.AZURE_ENDPOINT
 AZURE_KEY = settings.AZURE_KEY
@@ -15,21 +14,28 @@ LOCALE = settings.LANGUAGE_CODE
 ML_MODEL = "prebuilt-receipt"
 
 
-class BasicReceipt(BaseModel):
-    merchant_name: str
-    merchant_name_confidence: Decimal
-    transaction_date: str
-    transaction_date_confidence: Decimal
-
-
 def populate_receipt_model(receipt_model_id: int) -> None:
     receipt_model = Receipt.objects.get(id=receipt_model_id)
     analyzed_receipt_result = analyze_receipt_file(receipt_model.receipt_file.path)
 
-    receipt_dict = {}
     for _idx, receipt in enumerate(analyzed_receipt_result.documents):
         merchant_object = receipt.fields.get("MerchantName")
-        receipt_dict["merchant_name",] = merchant_object.value_string
+        merchant_name = merchant_object.value_string
+        merchant_name_confidence = merchant_object.confidence
+        transaction_object = receipt.fields.get("TransactionDate")
+        transaction_date = transaction_object.value_date
+        transaction_date_confidence = transaction_object.confidence
+
+        merchant_object, _ = Merchant.objects.get_or_create(
+            title=merchant_name, defaults={"verified": False}
+        )
+
+        receipt_model.merchant = merchant_object
+        receipt_model.merchant_confidence = Decimal(merchant_name_confidence)
+        receipt_model.transaction_date = transaction_date
+        receipt_model.transaction_date_confidence = Decimal(transaction_date_confidence)
+
+        receipt_model.save()
 
 
 @shared_task
