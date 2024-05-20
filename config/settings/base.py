6c3e398b33461fc04fc7c49db019a178f7a322c9
Karl Wooster <karl@woostertech.com>
2024-05-20 15:47:12 -0700

    build(troubleshooting): render: print DATABASES

--- config/settings/base.py
+++ config/settings/base.py
@@ -46,6 +46,8 @@ LOCALE_PATHS = [str(BASE_DIR / "locale")]
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {"default": env.db("DATABASE_URL")}
+
+print(f"DATABASES: {DATABASES}")
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 # https://docs.djangoproject.com/en/stable/ref/settings/#std:setting-DEFAULT_AUTO_FIELD
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
