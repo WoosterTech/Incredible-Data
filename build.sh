d25bb31b96f0a0c5c4d36a662bb404b2e319e484
Karl Wooster <karl@woostertech.com>
2024-05-21 08:19:21 -0700

    build(superuser): add super user creation to build script

--- build.sh
+++ build.sh
@@ -10,3 +10,12 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+# create superuser
+if [[ -z "${DJANGO_CREATE_SUPERUSER}" ]]; then
+  echo "No superuser created"
+else
+  python manage.py createsuperuser --noinput
+  export DJANGO_SUPERUSER_PASSWORD="invalid"
+  export DJANGO_CREATE_SUPERUSER="false"
+fi
