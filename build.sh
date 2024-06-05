8cfd50c5f3552a9ab66ee94413d1bdffc80afa13
Karl Wooster <karl@woostertech.com>
2024-06-05 10:43:21 -0700

    build("c"-group): remove "c" group from build.sh used by render

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-poetry install --with c,production
+poetry install --with production
 
 # Convert static asset files
 python manage.py collectstatic --no-input
